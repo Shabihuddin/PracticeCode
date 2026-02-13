39499957caf0145c5a95bf0a7c25f5f38d392672
DELL <DELL@Shabih>
2026-02-13 19:38:07 +0530

    First Commit

---
+++ c10/src/q10/Main.java
@@ -0,0 +1,28 @@
+package q10;
+
+public class Main {
+	public static void main(String[] args) {
+
+		String s1 = " Hello World ";
+		System.out.println(s1);
+		System.out.println(s1.length());
+
+		System.out.println(s1.compareTo(" Hello World "));
+
+		String s2 = " heyyy...";
+		System.out.println(s1.concat(s2));
+
+		System.out.println(s1.trim());
+
+		System.out.println(s1.replace('o', 'x'));
+
+		System.out.println(String.valueOf(10));
+
+		Integer integer = new Integer(10);
+		System.out.println(integer);
+
+		String string = new String("Shabih");
+		System.out.println(string);
+	}
+
+}
